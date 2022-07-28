b1923da507458b158cd1bd9bafab941199591a8e
Juanyonda13 <juan.yonda@misena.edu.co>
2022-07-28 10:26:19 -0500

    detalle_familia-parentescos-DB

---
+++ app/Http/Controllers/DetalleFamiliaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Detalle_familia;
+use Illuminate\Http\Request;
+
+class DetalleFamiliaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Detalle_familia  $detalle_familia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Detalle_familia $detalle_familia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Detalle_familia  $detalle_familia
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Detalle_familia $detalle_familia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Detalle_familia  $detalle_familia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Detalle_familia $detalle_familia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Detalle_familia  $detalle_familia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Detalle_familia $detalle_familia)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/HistoriaClinicaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Historia_clinica;
+use Illuminate\Http\Request;
+
+class HistoriaClinicaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Historia_clinica  $historia_clinica
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Historia_clinica $historia_clinica)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Historia_clinica  $historia_clinica
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Historia_clinica $historia_clinica)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Historia_clinica  $historia_clinica
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Historia_clinica $historia_clinica)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Historia_clinica  $historia_clinica
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Historia_clinica $historia_clinica)
+    {
+        //
+    }
+}
